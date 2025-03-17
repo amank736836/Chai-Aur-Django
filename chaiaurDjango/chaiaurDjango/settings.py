5b6c3cc8c4da1f745e09acc0fcc94af52d567614
Aman Kumar <amankarguwal0@gmail.com>
2025-03-17 22:37:29 +0530

    add django_browser_reload middleware and update settings

--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -41,10 +41,11 @@ INSTALLED_APPS = [
     "chai",
     "tailwind",
     "theme",
+    "django_browser_reload",
 ]
 
-TAILWIND_APP_NAME = 'theme'
-INTERNAL_IPS = ['127.0.0.1']
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = ["127.0.0.1"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloaderMiddleware",
 ]
 
 ROOT_URLCONF = "chaiaurDjango.urls"
