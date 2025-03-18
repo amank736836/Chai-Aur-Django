5ae595312f1b4d33f9c50b15d435f98f6e9299df
Aman Kumar <amankarguwal0@gmail.com>
2025-03-18 21:16:13 +0530

    add all_chai view to list ChaiVarity and create chai_detail view for individual chai details

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 
 def all_chai(request):
-    return render(request, "chai/all_chai.html")
+    chais = ChaiVarity.objects.all()
+    context = {
+        "chais": chais,
+    }
+    return render(request, "chai/all_chai.html", context)
+
+
+def chai_detail(request, id):
+    chai = get_object_or_404(ChaiVarity, id=id)
+    context = {
+        "chai": chai,
+    }
+    return render(request, "chai/chai_detail.html", context)
