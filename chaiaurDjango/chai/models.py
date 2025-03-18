ba247b01e2224b791adb963d510e800348ce1bb7
Aman Kumar <amankarguwal0@gmail.com>
2025-03-18 07:27:11 +0530

    add ChaiVarity model with type choices and date added field

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ("ML", "Masala Chai"),
+        ("KL", "Kadak Chai"),
+        ("CL", "Chai Latte"),
+        ("CL", "Green Chai"),
+        ("BL", "Black Chai"),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="chai/images")
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
