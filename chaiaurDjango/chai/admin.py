c57d39afa7d77556b5bd55e210956ca84c55f56a
Aman Kumar <amankarguwal0@gmail.com>
2025-03-19 06:14:06 +0530

    refactor admin registration for ChaiVariety and ChaiStore models

--- chaiaurDjango/chai/admin.py
+++ chaiaurDjango/chai/admin.py
@@ -16,11 +16,13 @@ class ChaiVarietyAdmin(admin.ModelAdmin):
 
 class ChaiStoreAdmin(admin.ModelAdmin):
     list_display = ("name", "location")
-    filter_horizontal = "chai_varities"
+    filter_horizontal = ["chai_varieties"]
 
 
 class ChaiCertificateAdmin(admin.ModelAdmin):
     list_display = ("chai", "certificate_number", "issue_date", "valid_until")
 
 
-admin.site.register(ChaiVarity)
+admin.site.register(ChaiVarity, ChaiVarietyAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
+admin.site.register(ChaiStore, ChaiStoreAdmin)
